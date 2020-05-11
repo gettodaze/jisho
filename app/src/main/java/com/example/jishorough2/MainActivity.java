ce15af29384a1ae3d68ec9f5bdac55ab08b8afc7
John McCloskey <john.gaoxing@gmail.com>
2020-05-11 01:05:20 -0500

    retrieve and parse api response, send text to box

--- app/src/main/java/com/example/jishorough2/MainActivity.java
+++ app/src/main/java/com/example/jishorough2/MainActivity.java
@@ -15,8 +15,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import static android.util.Log.d;
 
@@ -44,11 +48,40 @@ public class MainActivity extends AppCompatActivity {
         return super.onKeyUp(keyCode, event);
     }
 
-    private JSONObject jishoAPI(String query){
+    private ArrayList<Entry> jishoAPIHandler(String response) throws JSONException {
+        JSONObject responseJSON;
+        try {
+            responseJSON = new JSONObject(response);
+        } catch (JSONException e) {
+            throw new UnexpectedJSONException("Response not formatted in parseable JSON", e);
+        }
+        JSONArray data;
+        try {
+            data = responseJSON.getJSONArray("data");
+        } catch (JSONException e) {
+            throw new UnexpectedJSONException("Response did not include a data field",e);
+        }
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        for(int i=0; i<data.length(); i++){
+            JSONObject jsonEntry = data.getJSONObject(i);
+            JSONArray wordReadings = jsonEntry.getJSONArray("japanese");
+            JSONArray senses = jsonEntry.getJSONArray("senses");
+            for (int j = 0; j < senses.length(); j++) {
+                JSONObject sense = senses.getJSONObject(j);
+                ArrayList<String> eng_definitions = JSONParse.JSONArrayToStingList(
+                        sense.getJSONArray("english_definitions"));
+                String def_string = String.join(", ", eng_definitions);
+                entries.add(new Entry(wordReadings, def_string));
+            }
+
+        }
+        return entries;
 
     }
 
-    private void search(String query) {
+
+        private void search(String query) {
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://jisho.org/api/v1/search/words?keyword="+query;
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -56,21 +89,21 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         d("MainActivity", "Responding...");
-                        JSONObject responseJSON = null;
+
+                        ArrayList<Entry> entries = null;
                         try {
-                            responseJSON = new JSONObject(response);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            resultbox.setText("JSON could not be read!");
-                        }
-                        try {
-                            data = responseJSON.getJSONArray("data");
+                            entries = jishoAPIHandler(response);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                        data.
                         // Display the first 500 characters of the response string.
-                        resultbox.setText(response.substring(0,500));
+                        String[] entryStrings = new String[entries.size()];
+                        int i = 0;
+                        for(Entry e : entries){
+                            entryStrings[i] = e.toString();
+                            i += 1;
+                        }
+                        resultbox.setText(String.join("\n", entryStrings));
                     }
                 },
                 new Response.ErrorListener() {
@@ -81,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
                 });
         queue.add(stringRequest);
     }
+
 
 
 }
