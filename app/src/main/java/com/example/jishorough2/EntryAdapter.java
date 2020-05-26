19e8076b9a226cfa02b560cec6ff8a17ce95ded3
John McCloskey <john.gaoxing@gmail.com>
2020-05-25 22:29:46 -0500

    Change Lists Fragment to use TextViews, not Entry Views

--- app/src/main/java/com/example/jishorough2/EntryAdapter.java
+++ app/src/main/java/com/example/jishorough2/EntryAdapter.java
@@ -4,9 +4,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -16,9 +19,13 @@ public class EntryAdapter extends RecyclerView.Adapter<EntryAdapter.EntryViewHol
     public class EntryViewHolder extends RecyclerView.ViewHolder {
 
         public View itemView;
+        public TextView tvWord;
+        public TextView tvDef;
         public EntryViewHolder(View v ) {
             super(v);
             itemView = v;
+            tvDef = itemView.findViewById(R.id.textview_listdef);
+            tvWord = itemView.findViewById(R.id.textview_listword);
         }
 
     }
@@ -36,13 +43,9 @@ public class EntryAdapter extends RecyclerView.Adapter<EntryAdapter.EntryViewHol
 
     @Override
     public void onBindViewHolder(@NonNull EntryViewHolder holder, int position) {
-        EntryView ev_word = holder.itemView.findViewById(R.id.entryview_listword);
-        EntryView ev_def = holder.itemView.findViewById(R.id.entryview_listdef);
         Entry e = entries.get(position);
-        ev_word.setEntry(e);
-        ev_def.setEntry(e);
-        ev_word.setText(e.wordReadingsString());
-        ev_def.setText(e.getDefinition());
+        holder.tvWord.setText(e.wordReadingsString());
+        holder.tvDef.setText(e.getDefinition());
 
     }
 
